52ccdc75d6ac0ec9f7b44de529a28e6e8853c4f9
茶茶 <chachakko@outlook.com>
2022-09-08 19:47:03 +0800

    Cancel publish plugins

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,23 +1,5 @@
 plugins {
   id("shared")
-  id("java-gradle-plugin")
-}
-
-gradlePlugin {
-  plugins {
-    create("mavenPublishPlugin") {
-      id = "com.vanniktech.maven.publish"
-      implementationClass = "com.vanniktech.maven.publish.MavenPublishPlugin"
-      displayName = "Gradle Maven Publish Plugin"
-      description = "Gradle plugin that configures publish tasks to automatically upload all of your Java, Kotlin, Gradle, or Android libraries to any Maven instance."
-    }
-    create("mavenPublishBasePlugin") {
-      id = "com.vanniktech.maven.publish.base"
-      implementationClass = "com.vanniktech.maven.publish.MavenPublishBasePlugin"
-      displayName = "Gradle Maven Publish Base Plugin"
-      description = "Gradle plugin that configures publish tasks to automatically upload all of your Java, Kotlin, Gradle, or Android libraries to any Maven instance."
-    }
-  }
 }
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
